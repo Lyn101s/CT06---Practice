d78f512c77b75e7d5add152d41c044f01514f459
Lyn101s <lee.lin@computhink.com.sg>
2025-07-10 03:35:36 +0000

    Jul 10, 2025, 3:35 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -20,7 +20,7 @@ function draw() {
   let spacing = 50;
   for (let i=0; i<5; i++) {
     fill(i*50);
-    circle(50 + i*50, 100, 40);
+    circle(spacing + i*spacing, 100, 40);
   }
   // Task 1: Colour Gradient
 
