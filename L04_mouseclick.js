99d75494c19568ed6653f74b9412dfdbfd97e20b
Lyn101s <lee.lin@computhink.com.sg>
2025-07-14 05:34:07 +0000

    Jul 14, 2025, 5:34 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -5,9 +5,11 @@ function setup() {
 }
 
 function draw() {
-    for(let i=0; i<5; i++) {
-        fill(0, i*50, 0);
-        circle(50 + i*50, 50 + i*50, 40);
+    // for(let i=0; i<5; i++) {
+    //     fill(0, i*50, 0);
+    //     circle(50 + i*50, 50 + i*50, 40);
         
-    }
+    // }
+
+    
 }
