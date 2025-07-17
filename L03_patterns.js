db3fc47218895d7c305c4f1f348646656664be34
Lyn101s <lee.lin@computhink.com.sg>
2025-07-17 09:24:26 +0000

    Jul 17, 2025, 9:24 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -69,6 +69,7 @@ function draw() {
   let numOfCircle = 5;
   let totalDia = numOfCircle*circleDia;
   let startX = (width-totalDia)/2 + circleDia/2;
+  let startY = 
 
   for(let i=0; i<numOfCircle; i++) {
     circle( startX + i*circleDia , height/2, circleDia);
