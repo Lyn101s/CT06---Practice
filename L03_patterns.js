904705fdf9e0eec0578af3026465d0ba3360ea4a
Lyn101s <lee.lin@computhink.com.sg>
2025-07-17 08:31:14 +0000

    Jul 17, 2025, 8:31 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -3,8 +3,8 @@ function setup() {
     background(220);
     //noLoop(); // Stops continuous drawing
   }
-let colorValue = 0; //starting colorValue
-let spacing = 50;
+// let colorValue = 0; //starting colorValue
+// let spacing = 50;
 
 function draw() {
   // Recap 1: Repeating Circles
@@ -16,23 +16,54 @@ function draw() {
 
   // for (let i=0; i<5; i++) {
   //   fill(i*50);
-  //   circle(50 + i*50, 100, 40);
+  //   circle(50+ i*50, 100, 40);
   // }
 
 
  
   
   // for (let i=0; i<5; i++) {
-  // //   fill(i*50);
+  //   //fill(i*50);
   //   fill(colorValue);
   //   circle(spacing + i*spacing, 100, 40);
   //   colorValue +=1;
   // }
 
 
-  fill(colorValue);
-  circle(colorValue);
-  colorValue +=50;
+  // for (let i=0; i<5; i++) {
+  //   if(i%2 === 0) {
+  //     colorValue=0;
+  //   } else {
+  //     colorValue=255;
+  //   }
+  //   fill(colorValue);
+  //   circle(spacing + i*spacing, 100, 40);
+    
+  // }
+
+  //align group of circles to center
+  // let circleDia = 30;
+  // let numOfCircle = 5;
+  // let totalDia = numOfCircle*circleDia;
+  // let startX = (width-totalDia)/2 + circleDia/2;
+
+  // for(let i=0; i<numOfCircle; i++) {
+  //   circle( startX + i*circleDia , height/2, circleDia);
+
+  // }
+
+  // //align group of circles to left side. 
+  // let circleDia = 30;
+  // let numOfCircle = 5;
+  // let totalDia = numOfCircle*circleDia;
+  // let startX = circleDia/2;
+
+  // for(let i=0; i<numOfCircle; i++) {
+  //   circle( startX + i*circleDia , circleDia/2, circleDia);
+
+
+  // }
+
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
