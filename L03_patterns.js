c67ddfcdaf4f8e5e38ee36ee9b268de5eb5bec01
Lyn101s <lee.lin@computhink.com.sg>
2025-07-17 09:27:04 +0000

    Jul 17, 2025, 9:27 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -72,7 +72,7 @@ function draw() {
   let startY = (height - totalDia)/2 + circleDia/2;
 
   for(let j=0; j<numOfCircle; j++) {
-    let y = height
+    let y = height/2 + i*cir
     for(let i=0; i<numOfCircle; i++) {
       circle( startX + i*circleDia , height/2, circleDia);
 
