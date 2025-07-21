5ca2cdb5899a95ca4f188ecaf8cfba23575c993a
Lyn101s <lee.lin@computhink.com.sg>
2025-07-21 03:18:53 +0000

    Jul 21, 2025, 3:18 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -18,7 +18,7 @@ function setup() {
 
 function draw() {
     background('lightblue');
-    image(staticImage, 0, 0, 110, 133);
+    image(staticImage, spriteX, sprite110, 133);
 
     if(keyIsDown(LEFT_ARROW)) {
         spriteX -=5;
