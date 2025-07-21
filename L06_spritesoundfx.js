bf35c07d668476d711c438954eb5bfdfb75e6996
Lyn101s <lee.lin@computhink.com.sg>
2025-07-21 03:01:11 +0000

    Jul 21, 2025, 3:01 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -3,7 +3,7 @@
 let soundEffect, bgMusic, staticImage;
 
 function preload() {
-    soundEffect = loadSound('assests/pop.mp3');
+    soundEffect = loadSound('https://prod.liveshare.vsengsaas.visualstudio.com/join?195E29163997A27FCCDBD2AF8452B5222438');
     bgMusic = loadSound('assests/bossaNova.mps');
     staticImage = loadImage('assests/pico-a.png');
 }
